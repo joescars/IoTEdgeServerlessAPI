1451d5c107e569b269d84e4d8a8bb0bcbe1368c4
Joe Raio <joe.raio@microsoft.com>
2017-09-20 14:25:06 -0400

    Corrected commits

--- device_add/index.js
+++ device_add/index.js
@@ -1,11 +1,28 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = process.env.MyAccount_COSMOSDB;
+
 module.exports = function (context, req) {
+
     context.log('Add Device Triggered');
 
     // Add Device
     if (req.body) {
         let device = req.body;
         
-        //TODO: Write device to DB
+        // TODO: look into changing api so we can use binding
+        // context.bindings.out = device;
+
+        // write object to cosmosdb using mongo api
+        // TODO: data validation
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            insertDocument(db, device, function() {
+                db.close();
+                });
+            });
+
 
         context.res = {
             // status: 200, /* Defaults to 200 */
@@ -21,3 +38,11 @@ module.exports = function (context, req) {
     context.done();
 
 };
+
+var insertDocument = function(db, device, callback) {
+    db.collection('devices').insertOne(device, function(err, result) {
+        assert.equal(err, null);
+        //context.log("Inserted a document into the families collection.");
+        callback();
+    });
+    };
