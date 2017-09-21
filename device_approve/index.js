32875089b2d1d4715873cf3bba143ad9d75ff54b
Joe Raio <joraio@microsoft.com>
2017-09-20 21:48:26 -0400

    Device Update MongoDB

--- device_approve/index.js
+++ device_approve/index.js
@@ -1,22 +1,45 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MyAccount_COSMOSDB;
+
 module.exports = function (context, req) {
     context.log('Approve Device Triggered');
 
     // Approve Device
     if (req.body) {
         let device = req.body;
-        
-        //TODO: Update Record in DB
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Device Updated"
-        };
+        MongoClient.connect(url, function (err, db) {
+            var myquery = { id : device.id};
+            var newvalues = { id : device.id, name: device.name, approved: device.approved };
+            context.log(myquery);
+            context.log(newvalues);
+            db.collection("devices").updateOne(myquery, newvalues, function(err, res) {
+                db.close();
+                if(err) {
+                    context.res = {
+                        status: 400,
+                        body: "Error " + err,
+                        headers: { 'Access-Control-Allow-Origin': '*' }
+                    };                           
+                } else {
+                    //context.log(res);
+                    context.res = {
+                        status: 200,
+                        body: "Device Updated",
+                        headers: { 'Access-Control-Allow-Origin': '*' }
+                    };                        
+                }
+                context.done();
+            });
+   
+        });       
     }
     else {
         context.res = {
             status: 400,
             body: "Please pass a device in."
         };
+        context.done();
     }
-    context.done();
+    
 
 };
