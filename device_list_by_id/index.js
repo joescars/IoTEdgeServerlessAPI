6d261bfba60e322059a7d1a330e4c663ab046f4c
Joe Raio <joraio@microsoft.com>
2017-09-20 22:11:46 -0400

    Moving up db.close

--- device_list_by_id/index.js
+++ device_list_by_id/index.js
@@ -8,13 +8,13 @@ module.exports = function (context, req) {
     if (req.query.id) {
         MongoClient.connect(url, function (err, db) {
             db.collection('devices').findOne({id:req.query.id}, function (err, device) {
+                db.close();
                 context.res = {
                     status: 200,
                     body: {id:device.id,name:device.name,approved:device.approved},
                     headers: { 'Access-Control-Allow-Origin': '*' }
                 };
-                context.done();
-                db.close();
+                context.done();               
             });
     
         });
