a127346ca4b065ad0d4450d5e5e7e18a5a3eff2e
Joe Raio <joraio@microsoft.com>
2017-09-20 20:58:11 -0400

    List by ID pulls from DB

--- device_list_by_id/index.js
+++ device_list_by_id/index.js
@@ -1,26 +1,29 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MyAccount_COSMOSDB;
+
 module.exports = function (context, req) {
     context.log('Get Device Triggered');
 
     // Return single device by ID
     if (req.query.id) {
-
-        let device = {
-            id: req.query.id,
-            name: "Device " + req.query.id,
-            approved: true
-        };
-
-        context.res = {
-            status: 200,
-            body: device
-        };
+        MongoClient.connect(url, function (err, db) {
+            db.collection('devices').findOne({id:req.query.id}, function (err, device) {
+                context.res = {
+                    status: 200,
+                    body: {id:device.id,name:device.name,approved:device.approved},
+                    headers: { 'Access-Control-Allow-Origin': '*' }
+                };
+                context.done();
+                db.close();
+            });
+    
+        });
     }
     else {
         context.res = {
             status: 400,
             body: "Please pass an id in query"
         };
-    }
-    context.done();
-
+        context.done();
+    }    
 };
