5d9fca8261795fb2f62fe579136bfc3cbeedffed
Joe Raio <joraio@microsoft.com>
2017-09-20 22:08:58 -0400

    Minor update

--- get-devices/index.js
+++ get-devices/index.js
@@ -4,13 +4,13 @@ var url = process.env.MyAccount_COSMOSDB;
 module.exports = function (context, req) {
     MongoClient.connect(url, function (err, db) {
         db.collection('devices').find().toArray(function (err, devices) {
+            db.close();
             context.res = {
                 status: 200,
                 body: devices.map(d => ({ id: d.id, name: d.name, approved: d.approved })),
                 headers: { 'Access-Control-Allow-Origin': '*' }
             };
             context.done();
-            db.close();
         });
 
     });
