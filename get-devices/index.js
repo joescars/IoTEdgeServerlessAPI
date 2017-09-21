4050d88afbde7e2cafa2d615b78f6701bf60729f
Joe Raio <joraio@microsoft.com>
2017-09-21 10:34:36 -0400

    Ability to filter devices by approved

--- get-devices/index.js
+++ get-devices/index.js
@@ -2,8 +2,23 @@ var MongoClient = require('mongodb').MongoClient;
 var url = process.env.MyAccount_COSMOSDB;
 
 module.exports = function (context, req) {
+
+    var myQuery = {};
+
+    // filter: approve
+    if (req.query.approved) {
+        var appr = (req.query.approved == 'true');
+        if(appr) {
+            myQuery = { approved: "true"};
+        }
+        else {
+            myQuery = { approved: "false"};
+        }
+    }
+
+    // connect and get the data
     MongoClient.connect(url, function (err, db) {
-        db.collection('devices').find().toArray(function (err, devices) {
+        db.collection('devices').find(myQuery).toArray(function (err, devices) {
             db.close();
             context.res = {
                 status: 200,
